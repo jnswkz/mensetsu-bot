bc8cbd3aa5ec7c6da899681ff8de99b3fe76aa79
jnswkz <23520408@gm.uit.edu.vn>
2026-04-15 12:14:52 +0700

    feat(api): Implement fastapi for API response

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from mensetsu-bot!")
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
